67b94ad365aa8e143444c22218a97ad48525bbb9
caesarii <iwangqinghe@gmail.com>
2018-03-29 09:10:42 +0800

    create utils

---
+++ qing-flux/utils/utils.js
@@ -0,0 +1,20 @@
+
+function shallowArrayEqual(a, b) {
+    if(a === b) {
+        return true
+    }
+    
+    if(a.length !== b.length) {
+        return false
+    }
+    
+    for(let i = 0; i < a.length; i++) {
+        if(a[i] !== b[i]) {
+            return false
+        }
+    }
+    return true
+}
+
+
+module.exports = {shallowArrayEqual, }
