bd05c5220a32480c4cbaf519b06475d177ad415a
caesarii <iwangqinghe@gmail.com>
2018-03-29 11:25:24 +0800

    create test for subscriptions

--- qing-flux/container/FluxContainerSubscriptions.js
+++ qing-flux/container/FluxContainerSubscriptions.js
@@ -9,10 +9,13 @@ class FluxContainerSubscriptions {
         this._callbacks = []
         this._storeGroup = null
         this._stores = null
-        this.tokens = null
+        // 所有 store change 事件的回调句柄
+        this.tokens = []
     }
     
     setStores(stores) {
+        // subscriptions 是对 store 进行组织, 不考虑 state 的变化?
+        // 如果当前 stores 与之前的 stores 相同 (浅比较) 则不更新
         if(this._stores && shallowArrayEqual(this._stores, stores)) {
             return
         }
@@ -22,9 +25,12 @@ class FluxContainerSubscriptions {
         this._resetStoreGroup()
         
         let changed = false
-        const setChanged = () => { changed = true}
-        this._tokens = stores.map(store => store.addListener(setChanged))
+        // 注册所有 store 的 change 事件
+        this._tokens = stores.map(store => store.addListener(() => {
+            changed = true
+        }))
         
+        // 所有 store 更新完之后的回调
         const callCallbacks = () => {
             if(changed) {
                 this._callbacks.forEach(fn => fn())
