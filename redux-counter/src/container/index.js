be9e162c56247a355176fd6786f66d8cadab7573
caesarii <iwangqinghe@gmail.com>
2018-04-05 14:53:27 +0800

    create redux container

--- redux-counter/src/container/index.js
+++ redux-counter/src/container/index.js
@@ -1,26 +1,42 @@
-import {connect} from 'react-redux';
-import actionCreator from '../action/index';
+import actionCreator from '../action/index'
 import AppView from '../components/counter'
 import actionType from '../action/actionTypes'
+import React, { Component } from 'react'
 
-const mapStateToProps = (state) => {
-    return {
-        current: state.counterState,
+
+class App extends Component {
+    constructor (props) {
+        super(props)
+        this.store = props.store
+        // 初始状态
+        this.state = this.store.getState()
     }
-};
-
-const mapDispatchToProps = (dispatch) => ({
-    onClickCounter: (type, numer) => {
-        if(type === actionType.increase) {
-            dispatch(actionCreator.increaseNumber(numer));
-        } else if(type === actionType.decrease) {
-            dispatch(actionCreator.decreaseNumber(numer));
+    
+    render () {
+        const {store, state} = this
+        
+        // update
+        store.subscribe(() => {
+            this.setState(store.getState())
+            console.log('new state', store.getState())
+        })
+        
+        const onClickCounter = (type, number) => {
+            if (type === actionType.increase) {
+                store.dispatch(actionCreator.increaseNumber(number))
+            } else if (type === actionType.decrease) {
+                store.dispatch(actionCreator.decreaseNumber(number))
+            }
         }
+        
+        const dispatchers = {onClickCounter, }
+        
+        // 以这种形式传递 state 和 dispatchers 模仿 mapStateToProps, mapDispatcherToProps
+        return (
+          <AppView {...state} {...dispatchers}/>
+        )
     }
-});
+}
 
-// App
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(AppView)
+
+export default App
