3b86e8abb88a9ac673d1e2f9887ef67aee3203d0
caesarii <iwangqinghe@gmail.com>
2018-04-05 16:53:31 +0800

    create store

---
+++ flux-counter/src/store/index.js
@@ -0,0 +1,30 @@
+import actionTypes from '../action/actionTypes'
+import {ReduceStore} from 'flux/utils'
+import dispatcher from '../action/dispatcher'
+
+class CounterStore extends ReduceStore {
+    constructor() {
+        super(dispatcher)
+    }
+    
+    getInitialState() {
+        return {
+            current: 1
+        }
+    }
+    
+    reduce(state, action) {
+        console.log('reduce', action)
+        switch (action.type) {
+            case actionTypes.increase:
+                return {current: action.num + 1}
+            case actionTypes.decrease:
+                return {current: action.num - 1}
+            default:
+                return state;
+        }
+    }
+}
+
+
+export default new CounterStore()
