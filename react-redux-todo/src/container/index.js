009d04ab174e24d2f3e82e9942335d7d98070176
caesarii <iwangqinghe@gmail.com>
2018-04-09 11:39:42 +0800

    use bindActionCreators

--- react-redux-todo/src/container/index.js
+++ react-redux-todo/src/container/index.js
@@ -16,46 +16,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     const binds = bindActionCreators(action, dispatch)
     return binds
-    // return {
-    //     // //header
-    //     // onAdd(dispatch) {
-    //     //     return (draft) => {dispatch(action.addTodo(draft))}
-    //     // },
-    //     //
-    //     // onUpdateDraft(dispatch, draft) {
-    //     //     dispatch(action.updateDraft(draft))
-    //     // },
-    //     //
-    //     // // main
-    //     // onToggleAllTodos(dispatch) {
-    //     //     dispatch(action.toggleAllTodos())
-    //     // },
-    //     //
-    //     // onDeleteTodo(dispatch, id) {
-    //     //     dispatch(action.deleteTodo(id))
-    //     // },
-    //     //
-    //     // onEditTodo(dispatch, id, draft) {
-    //     //     dispatch(action.editTodo(id, draft))
-    //     // },
-    //     //
-    //     // onStartEditingTodo(dispatch, id) {
-    //     //     dispatch(action.startEditTodo(id))
-    //     // },
-    //     //
-    //     // onStopEditingTodo(dispatch) {
-    //     //     dispatch(action.stopEditTodo())
-    //     // },
-    //     //
-    //     // onToggleTodo(dispatch, id) {
-    //     //     dispatch(action.onToggleTodo(id))
-    //     // },
-    //     //
-    //     // // footer
-    //     // onDeleteCompletedTodos(dispatch) {
-    //     //     dispatch(action.deleteCompletedTodos())
-    //     // }
-    // }
+    
 }
 
 
