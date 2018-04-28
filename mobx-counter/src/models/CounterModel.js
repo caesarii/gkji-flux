0821cfd89150edf344ca54f86937ebc4aa9a8266
caesarii <iwangqinghe@gmail.com>
2018-04-28 11:12:12 +0800

    update

--- mobx-counter/src/models/CounterModel.js
+++ mobx-counter/src/models/CounterModel.js
@@ -3,11 +3,6 @@ import { observable, computed, action } from 'mobx'
 export default class CounterModel {
     @observable count = 0
     
-    // @computed
-    // get unfinishedTodoCount() {
-    //   return this.count.filter(todo => !todo.finished).length;
-    // }
-    
     @action
     increase (current) {
         this.count = current + 1
